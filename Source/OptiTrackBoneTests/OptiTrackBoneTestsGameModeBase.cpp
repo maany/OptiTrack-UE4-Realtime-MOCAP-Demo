096736100ee20dcb3a2d1a28c682b9bc7797e392
Christoph Schroeder <schroeder.c@cs.uni-bremen.de>
2017-06-21 17:56:38 +0200

    right arm hand mapping complete

---
+++ Source/OptiTrackBoneTests/OptiTrackBoneTestsGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OptiTrackBoneTests.h"
+#include "OptiTrackBoneTestsGameModeBase.h"
+
+
+
+
---
+++ Source/OptiTrackBoneTests/OptiTrackBoneTestsGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "OptiTrackBoneTestsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPTITRACKBONETESTS_API AOptiTrackBoneTestsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
